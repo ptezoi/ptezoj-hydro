cd6d64393849d01a120529c4f10c71e96963b62c
guke1024 <0124gj@gmail.com>
2022-08-03 17:16:49 +0800

    fix bug

--- packages/ui-default/modules.js
+++ packages/ui-default/modules.js
@@ -27,13 +27,13 @@ import './components/nprogress';
 import './components/monaco/loader';
 
 const modules = {
-    _, $, React, redux, ReactDOM,
+  _, $, React, redux, ReactDOM,
 };
 export default async function load(name) {
-    if (modules[name]) return modules[name];
-    if (name === 'echarts') return import('echarts');
-    if (name === 'moment') return import('moment');
-    throw new Error(`Module ${name} not found`);
+  if (modules[name]) return modules[name];
+  if (name === 'echarts') return import('echarts');
+  if (name === 'moment') return import('moment');
+  throw new Error(`Module ${name} not found`);
 }
 window.node_modules = { ...modules, load };
 window.$ = $;
