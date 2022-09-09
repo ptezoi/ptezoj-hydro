83b0f8156922e69bda42f4ef40c9c50f58b1a80a
guke1024 <0124gj@gmail.com>
2022-09-09 12:42:12 +0800

    vjudge: draft: 完成loj题面导入逻辑

--- packages/vjudge/src/providers/loj.ts
+++ packages/vjudge/src/providers/loj.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-await-in-loop */
+import { PassThrough } from 'stream';
 import { filter } from 'lodash';
 import * as superagent from 'superagent';
 import proxy from 'superagent-proxy';
@@ -17,30 +18,54 @@ export default class LOJProvider implements IBasicProvider {
     cookie: string[] = [];
 
     get(url: string) {
-        return url;
+        logger.debug('get', url);
+        if (!url.includes('//')) url = `${this.account.endpoint || 'https://loj.ac'}${url}`;
+        const req = superagent.get(url).set('Cookie', this.cookie);
+        if (this.account.proxy) return req.proxy(this.account.proxy);
+        return req;
     }
 
     post(url: string) {
         logger.debug('post', url, this.cookie);
         if (!url.includes('//')) url = `${this.account.endpoint || 'https://api.loj.ac.cn'}${url}`;
-        const req = superagent.post(url).set('Cookie', this.cookie).type('form');
+        const req = superagent.post(url).set('Cookie', this.cookie);
         if (this.account.proxy) return req.proxy(this.account.proxy);
         return req;
     }
 
     async getCsrfToken(url: string) {
-        return url;
+        const { header } = await this.get(url);
+        if (header['set-cookie']) {
+            await this.save({ cookie: header['set-cookie'] });
+            this.cookie = header['set-cookie'];
+        }
+        return '';
+    }
+
+    get loggedIn() {
+        return this.get('/').then(({ text: html }) => !html.includes('<button class="ui button _loginAndRegisterButton_1lx9b_30">登录</button>'));
     }
 
     async ensureLogin() {
-        return '';
+        if (await this.loggedIn) return true;
+        logger.info('retry login');
+        await this.getCsrfToken('/');
+        // await this.post('/login')
+        //     .set('referer', 'http://poj.org/')
+        //     .send({
+        //         user_id1: this.account.handle,
+        //         password1: this.account.password,
+        //         B1: 'login',
+        //         url: '/',
+        //     });
+        return this.loggedIn;
     }
 
     async getProblem(id: string) {
         logger.info(id);
         const result = await this.post('/api/problem/getProblem')
             .send({
-                displayId: id,
+                displayId: +id.split('P')[1],
                 localizedContentsOfAllLocales: true,
                 tagsOfLocale: 'zh_CN',
                 samples: true,
@@ -51,8 +76,11 @@ export default class LOJProvider implements IBasicProvider {
         if (!result.body.localizedContentsOfAllLocales) {
             return null;
         }
-        let content = '';
+        const contents = {};
+        const files = {};
+        const images = {};
         for (const c of result.body.localizedContentsOfAllLocales) {
+            let content = '';
             const sections = c.contentSections;
             for (const section of sections) {
                 if (section.type === 'Sample') {
@@ -69,9 +97,27 @@ ${result.body.samples[section.sampleId].outputData}
                     content += `\n${section.text}\n\n`;
                 }
             }
+            [...content.matchAll(/!\[\]\(.*\)/g)].forEach((ele) => {
+                const src = ele[0].match(/http(.*)[^)]+/)[0];
+                if (images[src]) {
+                    content = content.replace(src, `file://${images[src]}.png`);
+                    return;
+                }
+                const file = new PassThrough();
+                this.get(src).pipe(file);
+                const fid = String.random(8);
+                images[src] = fid;
+                files[`${fid}.png`] = file;
+                content = content.replace(src, `file://${fid}.png`);
+            });
+            if (content.includes('题目详见')) {
+                const problemId = content.match(/\/[0-9]+/)[0].replace('/', 'P');
+                content = `**题目详见 [LOJ ${problemId}](${problemId})，本题关闭提交！**`;
+            }
             let locale = c.locale;
             if (locale === 'en_US') locale = 'en';
             else if (locale === 'zh_CN') locale = 'zh';
+            contents[locale] = content;
         }
         const tags = result.body.tagsOfLocale.map((node) => node.name);
         const title = [
@@ -81,21 +127,32 @@ ${result.body.samples[section.sampleId].outputData}
             ),
             ...result.body.localizedContentsOfAllLocales,
         ][0].title;
-        const files = {};
+        const time = result.body.judgeInfo.timeLimit;
+        const memory = result.body.judgeInfo.memoryLimit;
         return {
             title,
             data: {
-                'config.yaml': Buffer.from(`target: ${id}`),
+                'config.yaml': Buffer.from(`time: ${time}\nmemory: ${memory}\ntype: remote_judge\nsubType: libreoj\ntarget: ${id}`),
             },
             files,
             tag: tags,
-            content: JSON.stringify(content),
+            content: JSON.stringify(contents),
         };
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async listProblem(page: number, resync = false) {
         return [];
+        // const skipCount = 0;
+        // const results = await this.post('/api/problem/queryProblemSet')
+        //     .send({
+        //         locale: 'zh_CN',
+        //         skipCount: skipCount,
+        //         takeCount: 50
+        //     });
+        // const res = results.body.result;
+        // const pli: string[] = Array.from(res.map((i) => `P${+i.meta.id.toString()}`));
+        // return pli;
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
