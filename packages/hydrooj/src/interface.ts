f9c09c059b52d9373ecb10dbbb61386c2df84ca3
guke1024 <0124gj@gmail.com>
2022-10-02 23:24:10 +0800

    core: interface: remove unused collections

--- packages/hydrooj/src/interface.ts
+++ packages/hydrooj/src/interface.ts
@@ -254,7 +254,7 @@ declare module './model/problem' {
 export type { ProblemDoc } from './model/problem';
 export type ProblemDict = NumericDictionary<ProblemDoc>;
 
-export interface StatusDoc {
+export interface StatusDocBase {
     _id: ObjectID,
     docId: any,
     docType: number,
@@ -262,7 +262,7 @@ export interface StatusDoc {
     uid: number,
 }
 
-export interface ProblemStatusDoc extends StatusDoc {
+export interface ProblemStatusDoc extends StatusDocBase {
     docId: number;
     docType: 10;
     rid?: ObjectID;
@@ -619,14 +619,12 @@ export interface OauthMap {
 
 export interface Collections {
     'blacklist': BlacklistDoc;
-    'contest': Tdoc;
     'domain': DomainDoc;
     'domain.user': any;
     'domain.union': DomainUnion;
     'record': RecordDoc;
     'document': any;
-    'document.status': any;
-    'problem': ProblemDoc;
+    'document.status': StatusDocBase;
     'user': Udoc;
     'stu.info': Student;
     'user.preference': UserPreferenceDoc;
@@ -643,9 +641,6 @@ export interface Collections {
     'oplog': OplogDoc;
     'event': EventDoc;
     'opcount': OpCountDoc;
-    'log': any;
-    'fs.chunks': any;
-    'fs.files': any;
 }
 
 export interface Model {
