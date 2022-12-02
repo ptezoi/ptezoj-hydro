b7cbaedb14666b5ef59e49c5c480cb6a23e652fb
guke1024 <0124gj@gmail.com>
2022-12-02 22:14:03 +0800

    merge hydro: core&ui: add discussion history

--- packages/hydrooj/src/interface.ts
+++ packages/hydrooj/src/interface.ts
@@ -415,11 +415,6 @@ export interface BlacklistDoc {
     expireAt: Date;
 }
 
-export interface HistoryDoc {
-    content: string;
-    time: Date;
-}
-
 // Discussion
 export type { DiscussionDoc } from './model/discussion';
 declare module './model/discussion' {
@@ -436,7 +431,8 @@ declare module './model/discussion' {
         updateAt: Date;
         nReply: number;
         views: number;
-        history: HistoryDoc[];
+        edited?: boolean;
+        editor?: number;
         react: Record<string, number>;
         sort: number;
         lastRCount: number;
@@ -452,16 +448,18 @@ export interface DiscussionReplyDoc extends Document {
     ip: string;
     content: string;
     reply: DiscussionTailReplyDoc[];
-    history: HistoryDoc[];
+    edited?: boolean;
+    editor?: number;
     react: Record<string, number>;
 }
 
 export interface DiscussionTailReplyDoc {
-    _id: ObjectID,
-    owner: number,
-    content: string,
-    ip: string,
-    history: HistoryDoc[],
+    _id: ObjectID;
+    owner: number;
+    content: string;
+    ip: string;
+    edited?: boolean;
+    editor?:number;
 }
 
 export interface BlogDoc {
@@ -612,6 +610,17 @@ export interface OauthMap {
     uid: number;
 }
 
+export interface DiscussionHistoryDoc {
+    title?: string;
+    content: string;
+    domainId: string;
+    docId: ObjectID;
+    /** Create time */
+    time: Date;
+    uid: number;
+    ip: string;
+}
+
 declare module './service/db' {
     interface Collections {
         'blacklist': BlacklistDoc;
@@ -621,6 +630,7 @@ declare module './service/db' {
         'record': RecordDoc;
         'document': any;
         'document.status': StatusDocBase;
+        'discussion.history': DiscussionHistoryDoc;
         'user': Udoc;
         'user.preference': UserPreferenceDoc;
         'vuser': VUdoc;
